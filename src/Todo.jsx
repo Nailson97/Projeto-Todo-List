e8407047ff6c04a99439a8d373548cc02adf0156
Nailson97 <nailsonsousa97@gmail.com>
2021-10-01 21:28:53 -0300

    pegando o valor do input

---
+++ src/Todo.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import './Todo.css'
+
+
+function Todo() {
+
+  const [text, setText] = useState('')
+ 
+  function handleChange(event) {
+      let t = event.target.value
+      setText(t)
+  }
+  
+
+  return (
+       <div className='container'>
+          <h1>Todo List</h1>
+          <form>
+             <input onCtype='text'  onChange={handleChange}></input>
+             <button>Adicionar</button>
+          </form>
+
+          <ul>
+            <li>{text}</li>
+          </ul>
+       </div>
+  )
+}
+
+export default Todo;
